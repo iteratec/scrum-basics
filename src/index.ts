e2a0bba00d90d3042b4dae7ce0ed1fcb30f0ebec
Christoph Murczek <christoph.murczek@iteratec.com>
2022-01-05 18:21:09 +0100

    scrum basics map

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
+bootstrapExtra().catch((e) => console.error(e));
 
 let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
+const time = new Date().toLocaleTimeString("de-de", { timeStyle: "short" });
+// const hours = today.getHours();
+// const minutes = today.getMinutes();
+// const time = `${hours < 10 ? `0${hours}` : hours}:${
+//   minutes < 10 ? `0${minutes}` : minutes
+// }`;
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
+WA.room.onEnterZone("clock", () => {
+  currentPopup = WA.ui.openPopup("clockPopup", time, []);
+});
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.room.onLeaveZone("clock", closePopUp);
 
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+function closePopUp() {
+  if (currentPopup !== undefined) {
+    currentPopup.close();
+    currentPopup = undefined;
+  }
 }
